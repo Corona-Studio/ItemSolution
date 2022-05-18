00f7f15cc958eb509e201c2783a98ce8ef46c4d1
leon <leonecho@outlook.com>
2022-05-18 15:55:53 +0800

    Start cleaning task when the number of items reaches the limit

--- src/main/java/top/leonx/itemsolution/ItemSolution.java
+++ src/main/java/top/leonx/itemsolution/ItemSolution.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class ItemSolution extends JavaPlugin {
     private static ItemSolution instance;
     private static ItemSolutionEventHandler itemSolutionEventHandler;
-    public static ItemSolutionConfig config = new ItemSolutionConfig();
+    public static ConfigManager config = new ConfigManager();
     public static Logger logger;
     FileConfiguration fileConfig = getConfig();
     public static ItemSolution getInstance() {
@@ -25,35 +25,13 @@ public class ItemSolution extends JavaPlugin {
     public void onEnable() {
         instance = this;
         logger = getLogger();
-        fileConfig.addDefault("clear-interval", 60);
-        fileConfig.addDefault("item-age-lowerLimit", 120);
-        fileConfig.addDefault("clear-item-not-on-ground", false);
-        fileConfig.addDefault("clear-item-with-custom-name", false);
-        fileConfig.addDefault("world-blacklist", new ArrayList<String>());
-        fileConfig.addDefault("use-whitelist", false);
-        fileConfig.addDefault("whitelist", new ArrayList<String>());
-        fileConfig.addDefault("use-blacklist", false);
-        fileConfig.addDefault("blacklist", new ArrayList<String>());
-        fileConfig.addDefault("send-message-to-player", true);
-        fileConfig.addDefault("message-when-start", "§6[ClearItem] §r§7Start clean items in %s.");
-        fileConfig.addDefault("message-when-finish", "§6[ClearItem] §r§7%d items has been cleaned.");
-        fileConfig.addDefault("send-count-down", true);
-        fileConfig.addDefault("count-down.first.message", "Dropped items will be cleaned after %d seconds.");
-        fileConfig.addDefault("count-down.first.interval", 5);
-        fileConfig.addDefault("count-down.first.start-time", 30);
-        fileConfig.addDefault("count-down.first.end-time", 10);
-        fileConfig.addDefault("count-down.second.message", "Dropped items will be cleaned after %d seconds!!!");
-        fileConfig.addDefault("count-down.second.interval", 2);
-        fileConfig.addDefault("count-down.second.start-time", 10);
-        fileConfig.addDefault("count-down.second.end-time", 2);
-        fileConfig.addDefault("use-world-override", false);
-        fileConfig.addDefault("world-override-setting", new ArrayList<String>());
+        ConfigManager.addDefaults(fileConfig);
         fileConfig.options().copyDefaults(true);
         saveConfig();
 
         itemSolutionEventHandler = new ItemSolutionEventHandler();
         getServer().getPluginManager().registerEvents(itemSolutionEventHandler, this);
-        Objects.requireNonNull(getCommand("clearitem")).setExecutor(new ItemSolutionCommand());
+        Objects.requireNonNull(getCommand("itemsolution")).setExecutor(new ItemSolutionCommand());
         refreshConfig();
     }
 
--- src/main/java/top/leonx/itemsolution/ItemSolutionEventHandler.java
+++ src/main/java/top/leonx/itemsolution/ItemSolutionEventHandler.java
@@ -20,126 +20,167 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @SuppressWarnings("unused")
 public class ItemSolutionEventHandler implements Listener {
-    private final HashMap<World, BukkitTask> worldClearTasks = new HashMap<>();
-
+    private final HashMap<World, BukkitTask> worldCheckTasks = new HashMap<>();
+    private final HashMap<World, BukkitTask> itemClearTasks = new HashMap<>();
     @EventHandler
     public void onServerLoaded(ServerLoadEvent event){
         var world= Bukkit.getServer().getWorld("world");
         var world_nether= Bukkit.getServer().getWorld("world_nether");
         var world_the_end= Bukkit.getServer().getWorld("world_the_end");
 
-        if(worldClearTasks.containsKey(world))
-            worldClearTasks.get(world).cancel();
-        if(worldClearTasks.containsKey(world_nether))
-            worldClearTasks.get(world_nether).cancel();
-        if(worldClearTasks.containsKey(world_the_end))
-            worldClearTasks.get(world_the_end).cancel();
+        if(worldCheckTasks.containsKey(world))
+            worldCheckTasks.get(world).cancel();
+        if(worldCheckTasks.containsKey(world_nether))
+            worldCheckTasks.get(world_nether).cancel();
+        if(worldCheckTasks.containsKey(world_the_end))
+            worldCheckTasks.get(world_the_end).cancel();
 
-        StartClearTask(world).ifPresent(bukkitTask -> worldClearTasks.put(world, bukkitTask));
-        StartClearTask(world_nether).ifPresent(bukkitTask -> worldClearTasks.put(world_nether, bukkitTask));
-        StartClearTask(world_the_end).ifPresent(bukkitTask -> worldClearTasks.put(world_the_end, bukkitTask));
+        StartCheckTask(world).ifPresent(bukkitTask -> worldCheckTasks.put(world, bukkitTask));
+        StartCheckTask(world_nether).ifPresent(bukkitTask -> worldCheckTasks.put(world_nether, bukkitTask));
+        StartCheckTask(world_the_end).ifPresent(bukkitTask -> worldCheckTasks.put(world_the_end, bukkitTask));
     }
     @EventHandler
     public void onWorldLoaded(WorldLoadEvent event) {
         World world = event.getWorld();
         // Scan every 1 minute
-        var task = StartClearTask(world);
+        var task = StartCheckTask(world);
         if(task.isPresent()) {
-            if (worldClearTasks.containsKey(world)) {
-                worldClearTasks.get(world).cancel();
+            if (worldCheckTasks.containsKey(world)) {
+                worldCheckTasks.get(world).cancel();
             }
-            worldClearTasks.put(world, task.get());
+            worldCheckTasks.put(world, task.get());
         }
     }
 
     @EventHandler
     public void onWorldUnloaded(WorldUnloadEvent event) {
         World world = event.getWorld();
-        if(worldClearTasks.containsKey(world)){
-            worldClearTasks.get(world).cancel();
-            worldClearTasks.remove(world);
+        if(worldCheckTasks.containsKey(world)){
+            worldCheckTasks.get(world).cancel();
+            worldCheckTasks.remove(world);
         }
     }
 
-    private Optional<BukkitTask> StartClearTask(World world){
+    private Optional<BukkitTask> StartCheckTask(World world){
         if(world==null){
             return Optional.empty();
         }
         if (ItemSolution.config.worldBlacklist.contains(world.getName())) {
             return Optional.empty();
         }
-
+        var settings = ItemSolution.config.getSetting(world.getName());
         BukkitScheduler scheduler = Bukkit.getScheduler();
         ItemSolution.getInstance().getLogger().info("Starting clear task for world: " + world.getName());
-        return Optional.of(scheduler.runTaskTimer(ItemSolution.getInstance(), new ClearItemRunnable(world), 20L, 20L));
+        return Optional.of(scheduler.runTaskTimer(ItemSolution.getInstance(), new CheckItemRunnable(world),
+                settings.checkInterval*20L, settings.checkInterval*20L));
     }
+    public static boolean CanBeCleaned(Item item,ConfigManager.Setting setting){
+        if(item.getTicksLived() < setting.itemAgeLowerLimit*20L)
+            return false;
 
+        // Does not clean up items that are not on the ground
+        if (!setting.clearItemNotOnGround && !item.isOnGround())
+            return false;
+
+        // Use a blacklist, and it's not in the blacklist, skip it
+        if(setting.useBlacklist && !setting.blackListMaterials.contains(item.getItemStack().getType())){
+            return false;
+        }
+
+        // Use whitelist, and it's in the whitelist, skip it
+        if(setting.useWhitelist && setting.whiteListMaterials.contains(item.getItemStack().getType())){
+            return false;
+        }
+
+        if(!setting.clearItemWithCustomName
+                && item.getItemStack().hasItemMeta()
+                && Objects.requireNonNull(item.getItemStack().getItemMeta()).hasDisplayName()){
+            return false;
+        }
+
+        return true;
+    }
     public static void DoClearWorldItem(World world){
-        ItemSolution.getInstance().getLogger().info("Scanning item entity in world: " + world.getName());
         AtomicLong count = new AtomicLong();
-        final ItemSolutionConfig.Setting setting = ItemSolution.config.getSetting(world.getName());
+        final ConfigManager.Setting setting = ItemSolution.config.getSetting(world.getName());
         final var startMessage = ChatColor.RED + setting.messageWhenStart.replaceAll("%world%", world.getName());
         world.getPlayers().forEach(player -> player.sendMessage(startMessage));
 
         world.getEntities().forEach(entity -> {
             if (entity instanceof Item item) {
-                if(item.getTicksLived() < setting.itemAgeLowerLimit*20L)
-                    return;
-
-                // Does not clean up items that are not on the ground
-                if (!setting.clearItemNotOnGround && !item.isOnGround())
-                    return;
-
-                // Use a blacklist and it's in the blacklist
-                if(setting.useBlacklist && !setting.blackListMaterials.contains(item.getItemStack().getType())){
-                    return;
+                if(CanBeCleaned(item,setting)){
+                    item.remove();
+                    ItemSolution.logger.finest(String.format("Item %s removed", item.getItemStack().getType()));
+                    count.getAndIncrement();
                 }
-
-                // Use whitelist and it's not in the whitelist
-                if(setting.useWhitelist && setting.whiteListMaterials.contains(item.getItemStack().getType())){
-                    return;
-                }
-
-                if(!setting.clearItemWithCustomName
-                        && item.getItemStack().hasItemMeta()
-                        && Objects.requireNonNull(item.getItemStack().getItemMeta()).hasDisplayName()){
-                    return;
-                }
-
-                item.remove();
-                ItemSolution.getInstance().getLogger().finest(String.format("Item %s removed", item.getItemStack().getType()));
-                count.getAndIncrement();
             }
         });
-        ItemSolution.getInstance().getLogger().info(String.format("%d items removed in world %s", count.get(), world.getName()));
+        ItemSolution.getInstance().getLogger().info(String.format("%d dropped items removed in world %s", count.get(), world.getName()));
         var finishMessage = ChatColor.RED +
                 setting.messageWhenFinish.replaceAll("%count%",String.valueOf(count.get()));
         world.getPlayers().forEach(player -> player.sendMessage(finishMessage));
     }
 
-    private static final class ClearItemRunnable implements Runnable {
+    private final class CheckItemRunnable implements Runnable {
+        private final World world;
+
+        CheckItemRunnable(World world) {
+            this.world = world;
+        }
+
+        @Override
+        public void run() {
+            var setting = ItemSolution.config.getSetting(world.getName());
+            // Count the number of items that can be cleaned in the world
+            long count = world.getEntities().stream().filter(entity -> entity instanceof Item && CanBeCleaned((Item) entity,setting)).count();
+            ItemSolution.logger.info(String.format("%d dropped items can be cleaned in world %s", count, world.getName()));
+            // If there are enough items that can be cleaned, start the clean task
+            if(count > setting.itemAmountTrigger){
+                if (itemClearTasks.containsKey(world)) { // If the task is already running, skip
+                    return;
+                }
+                ItemSolution.logger.info(String.format("Starting clean task for world %s", world.getName()));
+                // Start the clear task, and save the task to the map
+                // The task will count down the time, and when the time is up, start cleaning
+                // The task will be cancelled and remove from the map when the task is finished
+                BukkitScheduler scheduler = Bukkit.getScheduler();
+                var task = scheduler.runTaskTimer(ItemSolution.getInstance(), new ClearItemRunnable(world),
+                        0L, 20L);
+
+                itemClearTasks.put(world, task);
+            }
+        }
+    }
+    private final class ClearItemRunnable implements Runnable {
         private final World world;
 
         private ClearItemRunnable(World world) {
             this.world = world;
         }
 
-        private int sinceLastRun = 0;
+        private int timeSinceRun = 0;
         @Override
         public void run() {
-            sinceLastRun +=1;
-            ItemSolutionConfig.Setting setting = ItemSolution.config.getSetting(world.getName());
-            int remaining = setting.clearInterval - sinceLastRun;
+            timeSinceRun +=1;
+            ConfigManager.Setting setting = ItemSolution.config.getSetting(world.getName());
+
+            // Compute the time left
+            // Because there are two count down messages, we need to get the earlier one's start time to compute the time left
+            int remaining = Math.max(setting.cdFirstStartTime,setting.cdFirstEndTime) - timeSinceRun;
+
             ItemSolution.logger.finest("Running ClearItem task for world: %d seconds".formatted(remaining));
             int cdFirstStart = setting.cdFirstStartTime;
             int cdFirstEnd = setting.cdFirstEndTime;
             int cdFirstInterval = setting.cdFirstInterval;
+
+            // Within the time of the first countdown message, send the first count down message
             if(remaining <= cdFirstStart && remaining >= cdFirstEnd
                 &&(remaining - cdFirstEnd)% cdFirstInterval == 0){
                 var message = ChatColor.RED + setting.cdFirstMessage.replaceAll("%time%", String.valueOf(remaining));
                 world.getPlayers().forEach(player -> player.sendMessage(message));
             }
 
+            // Within the time of the second countdown message, send the second count down message
             int cdSecondStart = setting.cdSecondStartTime;
             int cdSecondEnd = setting.cdSecondEndTime;
             int cdSecondInterval = setting.cdSecondInterval;
@@ -150,8 +191,11 @@ public class ItemSolutionEventHandler implements Listener {
             }
 
             if(remaining<= 0){
+                // If the time is up, do the clean
                 DoClearWorldItem(world);
-                sinceLastRun=0;
+                BukkitScheduler scheduler = Bukkit.getScheduler();
+                itemClearTasks.get(world).cancel(); // cancel self
+                itemClearTasks.remove(world); // remove from map
             }
         }
     }
